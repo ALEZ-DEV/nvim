4ecc521b62cb58394b7824819da19f670ef704a0
ALEZ-DEV <raidersfocus@gmail.com>
2024-05-30 15:58:58 +0200

    initiale setup

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+-- treesitter.lua
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "elixir",
+        "heex",
+        "javascript",
+        "html",
+        "rust",
+        "ruby",
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+}
---
+++ lua/plugins/vim-agriculture.lua
@@ -0,0 +1,1 @@
+return { "jesseleite/vim-agriculture" }
