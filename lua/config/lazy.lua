5f2432feb7cdc2a175375e7a64d2c538321fd663
ALEZ-DEV <raidersfocus@gmail.com>
2024-07-14 18:39:06 +0200

    fix annoying error

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -3,7 +3,8 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
@@ -53,6 +54,3 @@ vim.keymap.set("n", "<leader>fh", telescope.help_tags, {})
 
 local fzf = require("fzf-lua")
 vim.keymap.set("n", "<leader><Space>", fzf.files, {})
-
-local agriculture = require("plugins.vim-agriculture")
-vim.keymap.set("n", "<leader>ss", agriculture.AgRawSearch, {})
