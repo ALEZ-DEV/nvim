69538c1cb70b1cd9dada39391fe3cb644a391e89
ALEZ-DEV <me@al3z.dev>
2025-04-16 14:58:45 +0200

    added rustaceanvim

---
+++ lua/ftplugin/rust.lua
@@ -0,0 +1,18 @@
+local bufnr = vim.api.nvim_get_current_buf()
+vim.keymap.set(
+  "n",
+  "K",  -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+  function()
+    vim.cmd.RustLsp({'hover', 'actions'})
+  end,
+  { silent = true, buffer = bufnr }
+)
+vim.keymap.set(
+  "n",
+  "D",
+  function()
+    vim.cmd.RustLsp('debug')
+  end,
+  { silent = true, buffer = bufnr }
+)
+
