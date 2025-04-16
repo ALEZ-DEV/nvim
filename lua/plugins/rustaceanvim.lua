79080715c59de430921a01652868218b42d64495
ALEZ-DEV <me@al3z.dev>
2025-04-16 20:25:39 +0200

    can now disable or enable rust formatter

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,5 +1,19 @@
 return {
-  'mrcjkb/rustaceanvim',
-  version = '^6', -- Recommended
+  "mrcjkb/rustaceanvim",
+  version = "^6", -- Recommended
   lazy = false, -- This plugin is already lazy
+  init = function()
+    require("lazyvim.util").lsp.on_attach(function(_, buffer)
+      -- stylua: ignore
+      vim.keymap.set("n", "<leader>cd", function()
+        vim.g.autoformat = false
+        vim.notify("Rust formatter disabled")
+      end, { buffer = buffer, desc = "Disable formatting" })
+
+      vim.keymap.set("n", "<leader>ce", function()
+        vim.g.autoformat = true
+        vim.notify("Rust formatter enabled")
+      end, { buffer = buffer, desc = "Enable formatting" })
+    end)
+  end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,35 +1,31 @@
 -- treesitter.lua
 return {
-  { import = "lazyvim.plugins.extras.lang.json" },
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-        ensure_installed = {
-          "c",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          "elixir",
-          "heex",
-          "javascript",
-          "html",
-          "rust",
-          "ruby",
-          "tsx",
-          "markdown",
-          "markdown_inline",
-          "yaml",
-        },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  }
+    configs.setup({
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "elixir",
+        "heex",
+        "javascript",
+        "html",
+        "rust",
+        "ruby",
+        "tsx",
+        "markdown",
+        "markdown_inline",
+        "yaml",
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
